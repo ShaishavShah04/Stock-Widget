aab21a11334eac4aa928b9426ccd5c12761f74d7
p.kolhe <p.kolhe@share.epsb.ca>
2020-06-10 15:18:23 -0600

    web scraper for news added

--- webscraping.py
+++ webscraping.py
@@ -13,8 +13,19 @@ def webScrapeURL(url):
     entirePage = BeautifulSoup(response.content, 'html.parser') # converts content into html code
     return entirePage
 
+def getNewsStock(page):
+    newsArray = []
+    for data in page.findAll("div", {"class": "news-link-container"}): # searches for news class in entire page
+        newsText = data.findAll(text=True) # returns all text and publisher
+        newsLink = data.find('a').get('href') # search for links
+        newsArray.append([newsText[0], newsText[1], newsLink]) # append to array to be used to find news sources
+    return newsArray
+
 
 if __name__ == '__main__':
     applePage = webScrapeURL("https://finviz.com/quote.ashx?t=AAPL")
+    news = getNewsStock(applePage)
+    for i in range(len(news)):
+        print(news[i])
 
 
