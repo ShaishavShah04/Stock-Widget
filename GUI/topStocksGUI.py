e0721bd4d48e639a7ba51d869cddf89c9e592f77
p.kolhe <p.kolhe@share.epsb.ca>
2020-06-11 16:12:31 -0600

    gui folder added

---
+++ GUI/topStocksGUI.py
@@ -0,0 +1,13 @@
+'''
+title: GUI for top stocks
+author: Palaash Kolhe
+date created: 2020-06-11
+'''
+
+from tkinter import *
+from top_stocks import TopStocks
+
+class topStocksGUI:
+    def __init__(self):
+        self.topStocks = []
+
