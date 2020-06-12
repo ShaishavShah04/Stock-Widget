ba7218feb8fef5f3b93fd784beb444b920254710
p.kolhe <p.kolhe@share.epsb.ca>
2020-06-12 15:21:13 -0600

    made searchbar gui use .grid rather than .pack

--- GUI/searchBarGUI.py
+++ GUI/searchBarGUI.py
@@ -1,4 +1,3 @@
-
 from tkinter import *
 
 class SearchBarGUI:
@@ -10,16 +9,24 @@ class SearchBarGUI:
 
         # Frame
         self.searchBarFrame = Frame(window, height = 55, width = 600, pady = 5, bg=self.searchBarBackground)
-        self.searchBarFrame.grid(row=1, column=1, columnspan=3, sticky='n')
-        self.searchBarFrame.columnconfigure(0, weight=2)
+        self.searchBarFrame.grid(row=1, column=1, columnspan=7, sticky='n')
+        # self.searchBarFrame.columnconfigure(0, weight=2)
 
         # Label, Input, And Submit
         self.tickerLabel = Label(self.searchBarFrame, text="Enter Ticker: ", font=('Helvetica', '12', 'bold'),bg=self.searchBarBackground, fg=self.searchBarForeground)
-        self.tickerLabel.pack(side=LEFT, fill=BOTH, expand=False, pady=5, padx=20)
-        self.searchBar = Entry(self.searchBarFrame, bg='gray22', width=45, fg=self.searchBarForeground,font=('Helvetica', '12', 'bold'), borderwidth=0)
-        self.searchBar.pack(side=LEFT, expand=True, fill=BOTH)
+        # self.tickerLabel.pack(side=LEFT, fill=BOTH, expand=False, pady=5, padx=20)
+        self.tickerLabel.grid(row=1, column=1, sticky='news', pady=5, padx=(20, 5))
+        self.tickerLabel.grid_propagate(0)
+
+        self.searchBar = Entry(self.searchBarFrame, bg='gray22', width=46, fg=self.searchBarForeground,font=('Helvetica', '12', 'bold'), borderwidth=0)
+        # self.searchBar.pack(side=LEFT, expand=True, fill=BOTH)
+        self.searchBar.grid(row=1, column=3, sticky='news')
+        self.searchBar.grid_propagate(0)
+
         self.searchButton = Button(self.searchBarFrame, command=self.submitClick, bg='gray16', fg=self.searchBarForeground, text='Search',font=('Helvetica', '12', 'bold'), borderwidth=0, width=15)
-        self.searchButton.pack(side=LEFT, expand=True, fill=BOTH)
+        # self.searchButton.pack(side=LEFT, expand=True, fill=BOTH)
+        self.searchButton.grid(row=1, column=5, sticky='news')
+        self.searchButton.grid_propagate(0)
 
     # Submit button:
     def submitClick(self):
