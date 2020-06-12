9097ee950122d86decf12d7a65636022e1c321e4
p.kolhe <p.kolhe@share.epsb.ca>
2020-06-11 18:48:49 -0600

    window gui created

---
+++ GUI/windowGUI.py
@@ -0,0 +1,27 @@
+'''
+title: GUI for Window
+authors: Palaash & Shaishav
+date created: 2020-06-11
+'''
+
+from tkinter import *
+from GUI import topStocksGUI
+
+class Window:
+    def __init__(self):
+        self.window = Tk()
+        self.window.minsize(1028, 720)
+        self.window.title("Stock Trader")
+        self.window.configure(bg="black")
+        self.window.resizable(False, False)
+
+        topStocks = topStocksGUI.TopStocksGUI(self.window)
+
+    # Accessor Methods #
+    def getWindow(self):
+        return self.window
+
+if __name__ == "__main__":
+    window = Window()
+    window.window.mainloop()
+
