faf89ceb9101ea5ecf6480cbbbcf948c353e149a
p.kolhe <p.kolhe@share.epsb.ca>
2020-06-12 11:37:07 -0600

    tops stocks completed

--- GUI/topStocksGUI.py
+++ GUI/topStocksGUI.py
@@ -25,30 +25,63 @@ class TopStocksGUI:
         self.backgroundColor = 'gray12'
         self.foregroundColor = 'white'
 
-        self.topStockFrame = Frame(window, height=360, width=425, pady = 5, bg = self.backgroundColor)
+        self.topStockFrame = Frame(window, height=346, width=425, bg = self.backgroundColor)
         self.topStockFrame.grid(row=1, column=8)
 
         self.column = 0
         self.row = 1
 
+        self.titleLabel = Label(self.topStockFrame, text='TOP STOCKS', font=('Helvetica', '12', 'bold'), bg=self.backgroundColor, fg=self.foregroundColor, padx=10, pady=10.5)
+        self.titleLabel.grid(columnspan=7, sticky='news')
+
+        stockBackground = 'gray22'
+
         for i in range(10):
-            self.label = Label(self.topStockFrame, text=self.stocks[i][0], font=('Helvetica', '12', 'bold'), bg='gray22', fg=self.foregroundColor, padx=5, pady=5)
-            self.label.grid(column=self.column, row=self.row)
+            self.label = Label(self.topStockFrame, text=self.stocks[i][0], font=('Helvetica', '10', 'bold'), bg=stockBackground, fg=self.foregroundColor, padx=5, pady=5)
+            self.label.grid(column=self.column, row=self.row, sticky='news')
             self.column += 1
 
-            self.label = Label(self.topStockFrame, text='CHANGE:', font=('Helvetica', '12', 'bold'), bg='gray22', fg=self.foregroundColor, padx=5, pady=5)
-            self.label.grid(column=self.column, row=self.row)
+            self.label = Label(self.topStockFrame, text='LAST:', font=('Helvetica', '10', 'bold'),
+                               bg=self.backgroundColor, fg=self.foregroundColor, padx=5, pady=5)
+            self.label.grid(column=self.column, row=self.row, sticky=W)
             self.column += 1
 
-            self.label = Label(self.topStockFrame, text=self.stocks[i][2], font=('Helvetica', '12', 'bold'), bg='gray22', fg=self.foregroundColor, padx=5, pady=5)
-            self.label.grid(column=self.column, row=self.row)
+            self.label = Label(self.topStockFrame, text=self.stocks[i][1], font=('Helvetica', '10'),
+                               bg=self.backgroundColor, fg=self.foregroundColor, padx=5, pady=5)
+            self.label.grid(column=self.column, row=self.row, sticky=W)
             self.column += 1
+
+            self.label = Label(self.topStockFrame, text='CHANGE:', font=('Helvetica', '10', 'bold'), bg=self.backgroundColor, fg=self.foregroundColor, padx=5, pady=5)
+            self.label.grid(column=self.column, row=self.row, sticky=W)
+            self.column += 1
+
+            if '-' in self.stocks[i][2]:
+                changeColour = 'red'
+            else:
+                changeColour = 'green'
+
+            self.label = Label(self.topStockFrame, text=self.stocks[i][2], font=('Helvetica', '10', 'bold'), bg=self.backgroundColor,fg=changeColour, padx=5, pady=5)
+            self.label.grid(column=self.column, row=self.row, sticky=W)
+            self.column += 1
+
+            self.label = Label(self.topStockFrame, text='VOLUME:', font=('Helvetica', '10', 'bold'),
+                               bg=self.backgroundColor, fg=self.foregroundColor, padx=4, pady=5)
+            self.label.grid(column=self.column, row=self.row, sticky=W)
+            self.column += 1
+
+            self.label = Label(self.topStockFrame, text=self.stocks[i][3], font=('Helvetica', '10'),
+                               bg=self.backgroundColor, fg=self.foregroundColor, padx=4, pady=5)
+            self.label.grid(column=self.column, row=self.row, sticky=W)
+            self.column += 1
+
+            if stockBackground == 'gray22':
+                stockBackground = self.backgroundColor
+            else:
+                stockBackground = 'gray22'
 
             self.row += 1
             self.column = 0
 
-
-        self.topStockFrame.columnconfigure(1, weight=1)
 
         self.topStockFrame.grid_propagate(0)
 
